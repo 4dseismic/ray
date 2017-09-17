449539a68f773f944dc8c84cb713d591f79aca1b
Einar Kjartansson <eik.klaki.net>
2017-09-17 12:07:57 +0000

    work on readphase.c

--- readeve.c
+++ readeve.c
@@ -20,13 +20,15 @@ void readEFile( char *ename )
 	FILE *efile ;
 	char *bp, *cp ;
 	static struct tm tm ;
-	time_t tt, ttEvent ;
+	time_t ttIndex, ttEvent,ttPhase ;
 	int indexms;
 	static size_t n=100 ;
 	int len ;
 	char line[100], buffer[100] ;
-	double fsec, sourceTime ;
+	double eventSec, sourceTime,phaseSec,eventFrac,phaseFrac,dist,ttime ;
 	static Solution sol ;
+	static Phase phase ;
+	Station *sp ;
 	cp=strncpy(buffer,ename,100) ;
 	printf("%s",ename) ; 
 	while(*cp){ 
@@ -43,7 +45,7 @@ void readEFile( char *ename )
 	tm.tm_year=atoi(bp-27)-1900 ;
 	sol.index = (((1900+tm.tm_year)*100+1+tm.tm_mon)*100+tm.tm_mday)*100+tm.tm_hour ;
 	sol.index = ((sol.index*100+tm.tm_min)*100+tm.tm_sec)*1000+indexms ; 
-	tt = mktime(&tm) ;
+	ttIndex = mktime(&tm) ;
 	cp = asctime(&tm) ;
 	printf("%ld ",sol.index) ;
 	printf("%s %s ",cp,bp);
@@ -56,19 +58,35 @@ void readEFile( char *ename )
 	tm.tm_mday= atoi(line+18) ;
 	tm.tm_hour= atoi(line+23) ;
 	tm.tm_min = atoi(line+27) ;
-	tm.tm_sec = 0 ;
+	eventSec = atof(line+31) ;
+	tm.tm_sec = trunc(eventSec) ; 
+	eventFrac = eventSec - tm.tm_sec ;
 	ttEvent = mktime(&tm) ;
-	fsec = atof(line+31) ;
-	sourceTime = (ttEvent-tt) + fsec ;
-	printf("fsec = %f sourceTime = %f \n",fsec,sourceTime) ;
+	sourceTime = (ttEvent-ttIndex) + eventSec ;
+	printf("eventSec = %f sourceTime = %f \n",eventSec,sourceTime) ;
 	cp = fgets(line,n,efile) ; sol.lat = atof(line+10) ;
 	cp = fgets(line,n,efile) ; sol.lon = atof(line+10) ;
 	fgets(line,n,efile) ; sol.depth = atof(line+12) ;
 	printf("lat lon depth: %f %f %f\n",sol.lat,sol.lon,sol.depth) ;
 	cp = fgets(line,n,efile) ;
 	cp = fgets(line,n,efile) ;
-	while (*cp != '0' ) {
-		printf(cp) ;
+	while (*cp == ' ' ) {
+		printf(cp) ; 
+		tm.tm_hour = atoi(line+6) ;
+		tm.tm_min  = atoi(line+9) ;
+		phaseSec = atof(line+12);
+		tm.tm_sec =  trunc(phaseSec) ;
+		phaseFrac = phaseSec - tm.tm_sec ;
+		ttPhase = mktime(&tm) ;
+		dist = atof(line+31) ;
+		ttime = ttPhase-ttEvent+phaseFrac-eventFrac ;
+		line[4] = 0 ;
+		sp = lookUpStation(line+1) ;
+		phase.index = sol.index ;
+		phase.pTime = ttPhase-ttIndex + phaseFrac ;
+		phase.type = line[5] ;
+		printf(" hour,min sec ttime %d %d %f %d %f %f %f %s\n",
+			tm.tm_hour,tm.tm_min,phaseSec,ttPhase-ttEvent,dist,ttime,dist/ttime,sp->name) ;
 		cp = fgets(line,n,efile) ;
 	}
 }
@@ -86,7 +104,7 @@ void getEList( char *rootname )
 		len = getline(&line,&n,efiles) ;
 		readEFile(line) ;
 /*		printf("%d %d _ %s",len, n,line) ; */
-		return ;
+		return ; 
 	} while (len > 0) ;
 	
 }
